f9bd9bfdcd8421bc81349caa30238002372c007e
Zuleto1231 <mzueta@unal.edu.co>
2023-11-06 18:29:35 -0500

    Primer commit

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -2,24 +2,27 @@ package gui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
 
 public class Calculadora extends Application  {
 	
 	public static void main(String args[]){ 
-	      ***(args); 
+	      launch(args); 
 	} 
 	
 	@Override
 	public void start(Stage window) throws Exception {
-		*** vb = new VBox();
-		window.setTitle(***);
-		
+		VBox vb = new VBox();
+		window.setTitle("Calculadora");
 		vb.getChildren().add(new Calculator());
-		Scene sc = new Scene(***, 270, 270);
-		window.setScene(***);
-		***.show();
+		Scene sc = new Scene(vb, 270, 270);
+		window.setScene(sc);
+		window.show();
+
+		// Establecer un color de fondo de púrpura más claro
+		sc.getRoot().setStyle("-fx-background-color: #9370DB;");
 	}
 	
 }
